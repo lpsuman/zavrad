e66cc130acf2bef632abb97cab1495e1a9b39f61
Luka Suman <luka.suman@fer.hr>
2017-04-11 00:23:37 +0200

    Initial commit

---
+++ desktop/src/hr/fer/lukasuman/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package hr.fer.lukasuman.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import hr.fer.lukasuman.AutomataGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AutomataGame(), config);
+	}
+}
